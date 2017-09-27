1eb284df0ac2ae0bbbc64756e5337177e7929b8d
Fran250581 <[fran250581@hotmail.es]>
2017-09-27 15:27:36 +0200

    Commit 2

--- app/src/main/java/com/example/fran/com/holamundo/MainActivity.kt
+++ app/src/main/java/com/example/fran/com/holamundo/MainActivity.kt
@@ -2,22 +2,18 @@ package com.example.fran.com.holamundo
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
 
+
     override fun onCreate(savedInstanceState: Bundle?) {
+        Log.d("Hola Mundo", "Entramos en onCreate()")
         super.onCreate(savedInstanceState)
 
-
-
-        val errorCompiled: Int = null //Esto no compilara
-
-        var text: String = "I'm a String, Everything is Ok" //Asignacion valida
-
-        text = null //Generara el mismo error de compilacion
-
-
-
+        //Se introduce un error para depurarlo con logcat
+        var error: Int? = null
+        if(0 >= error!!){ }
 
         setContentView(R.layout.activity_main)
     }
